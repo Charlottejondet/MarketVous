843abe595882b71bc0813229d97b2ae2e810d659
jondetc <jondetc@PC-B102-08.iut-valence.fr>
2015-05-13 18:00:23 +0200

    Create student class, which choose spinneret type

---
+++ MarkEtVous/src/MarkEtVous/Student.java
@@ -0,0 +1,13 @@
+package MarkEtVous;
+
+public class Student 
+{
+	
+	private Spinneret spinneretType;
+	
+	public Student(spinneretType type) 
+	{
+		this.spinneretType=new Spinneret(type);
+	}
+	
+}
