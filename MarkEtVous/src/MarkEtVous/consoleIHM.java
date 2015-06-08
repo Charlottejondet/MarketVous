93463372389c1a8aff28c1109e666e23e49a2866
jondetc <jondetc@PC-B123-03.iut-valence.fr>
2015-06-08 10:24:44 +0200

    create console interface, implements IHM

---
+++ MarkEtVous/src/MarkEtVous/consoleIHM.java
@@ -0,0 +1,23 @@
+package MarkEtVous;
+
+import java.util.Scanner;
+
+public class consoleIHM implements IHM{
+
+	@Override
+	public void chooseSpinneret() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void entryMark() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez saisir une note :");
+		String str = sc.nextLine();
+		
+	}
+	
+	
+
+}
